4e47db37d6e83c0441ab4ca1cd92d080d9b3513f
Milan Koirala <koiralamilon67@gmail.com>
2025-07-15 12:09:12 +0545

    update code style on test operation

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,22 +1,34 @@
-from calculator.operations import *
+from calculator.operations import add
+from calculator.operations import subtract
+from calculator.operations import multiply
+from calculator.operations import divide
+from calculator.operations import power
+from calculator.operations import modulus
+
 
 def test_add():
     assert add(2, 3) == 5
 
+
 def test_subtract():
     assert subtract(5, 2) == 3
+
 
 def test_multiply():
     assert multiply(4, 3) == 12
 
+
 def test_divide():
     assert divide(10, 2) == 5
+
 
 def test_divide_by_zero():
     assert divide(5, 0) == "Error: Cannot divide by zero"
 
+
 def test_power():
     assert power(2, 3) == 8
+
 
 def test_modulus():
     assert modulus(10, 3) == 1
